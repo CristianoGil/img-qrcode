a96e27662bdadb50639d5a85992bd3ccd5ba86ac
Cristiano Joao <gil.cristiano219@gmail.com>
2017-03-31 10:55:20 +0200

    To access reactive data context of the template instance
    Sorry for my bad english.
    I have used this simple and excellent package, However, When I try to pass text to generate QR Code in a reactive way, does not update the image. That is way, I've made some changes to improve this.

--- client/QrCode.js
+++ client/QrCode.js
@@ -4,28 +4,44 @@ function convertCanvasToImage(canvas) {
     return image;
 }
 
-Template.QrCode.onRendered(function () {
-    var instance = Template.instance(),
-        // Get the current smart argument
-        view = Blaze.getView(),
-        data = Blaze.getData(view);
+Template.QrCode.onRendered(function() {
+    var instance = this;
+    // Get the current smart argument
+    this.autorun(() => {
+        var view = Blaze.getView(),
+            data = Template.currentData(); // To access reactive data context of the template instance
+
+        instance.$("div").qrcode({
+            "render": 'canvas',
+            "size": 50,
+            "text": data.text
+        });
+
+
+        // First Check if exists an tag image
+        // If exist remove it
+        var imgOld = instance.$('div.qr-code-container').find('img');
+        if (imgOld.length > 0) {
+            for (var i = 0; i < imgOld.length; i++) {
+                instance.$(imgOld[i]).remove();
+            }
+        }
         
-    instance.$("div").qrcode({
-        "render": 'canvas',
-        "size": 50,
-        "text": data.text
+        var canvas = instance.$('div > canvas')[0];
+
+        instance.$("div").append(convertCanvasToImage(canvas));
+        instance.$("canvas").remove();
+
+        instance.$("img").addClass("qr-code");
+
+        if (instance.data.size !== undefined) {
+            instance.$("img").css({
+                'width': instance.data.size,
+                'height': instance.data.size
+            });
+        }
     });
 
-    var canvas = instance.$('div > canvas')[0];
-    instance.$("div").append(convertCanvasToImage(canvas));
-    instance.$("canvas").remove();
 
-    instance.$("img").addClass("qr-code");
 
-    if (instance.data.size !== undefined) {
-        instance.$("img").css({
-            'width': instance.data.size,
-            'height': instance.data.size
-        });
-    }
 });
